db538564ab0f1f3cb9ca04aa97950fce2f515dd5
flip154 <flip.400g@gmail.com>
2026-09-14 15:28:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
- <?php
- echo "<h1> oi </h1>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1> oi </h1>";
+    ?>
+</body>
+</html>
