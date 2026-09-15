cfdc6d148964e147fafe4c237f74524cd07fa638
flip154 <flip.400g@gmail.com>
2026-09-15 15:04:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
             <form action="Get/Post" method="get">
                 <input type="text" id="nome" name="nome" required>
                 <input type="number" id="idade" name="idade" required>
+                <button type="submit">enviar</button>
             </form>
             <h1>nome: <?= $nome ?></h1>
             <p>idade: <?= $idade ?></p>
