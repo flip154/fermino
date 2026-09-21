3c13fdd89f6aff4f477de5bf75cc9e67097e8dff
flip154 <flip.400g@gmail.com>
2026-09-21 15:12:49 -0300

    Update notas.php

--- notas.php
+++ notas.php
@@ -14,6 +14,11 @@
         if($_SERVER["REQUEST_METHOD"]=="POST"){
             $nome = $_POST["nome"];
             $idade = $_POST["idade"];
+            $nota1 = $_POST["nota1"];
+            $nota2 = $_POST["nota2"];
+            $nota3 = $_POST["nota3"];
+            $nota4 = $_POST["nota4"];
+            $nota5 = $_POST["nota5"];
         }
 ?>
     <!DOCTYPE html>
